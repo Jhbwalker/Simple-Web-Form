a6730ac51436736d28a6db9ce64e4b3d4deb357a
Jarrell H Walker <jwalker-burnam@mail.csuchico.edu>
2016-09-01 16:51:34 -0700

    Contact Form Page
    Basic Contact form using html5 php and ajax. Learning sending forms
    without reloading pages/

---
+++ ContactParser.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarrel
+ * Date: 9/1/2016
+ * Time: 4:45 PM
+ */
+if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m'])){
+    $e = $_POST['n'];
+    $e = $_POST['e'];
+    $m = nl2br($_POST['m']);
+    $to = "Jarrel62@gmail.com";
+    $from = $e;
+    
+
+}
+
+
+
+?>
